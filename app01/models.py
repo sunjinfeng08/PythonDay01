18f31e31a7c9356868e0aa20942558837fc26c57
sunjinfeng08 <sunjinfeng08@163.com>
2024-09-04 13:50:51 +0800

    Initial commit

--- app01/models.py
+++ app01/models.py
@@ -22,3 +22,7 @@ class UserInfo(models.Model):
         (2,"女")
     )
     gender = models.SmallIntegerField(verbose_name="性别",choices=gender_choices)
+
+class Admin(models.Model):
+    username = models.CharField(verbose_name="用户名",max_length=32)
+    password = models.CharField(verbose_name="密码",max_length=64)
